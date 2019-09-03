47fb68703f756b6a09fac9cdf74e06f71d6947df
Landry Irakoze <irakozel@gmail.com>
2019-09-03 15:29:01 -0400

    crud rest client in routes

---
+++ posts/postRoutes.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const router = express.Router();
+const DB = require('./data/db');
+
+router.get('/', (req, res) => {
+    DB.find()
+        .then(db => {
+            res.status(200).json(db)
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'error retrieving list of users' })
+        })
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (id) {
+        DB.find(id)
+        .then(db => {
+            res.status(200).json(db)
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'error retrieving user' })
+        })
+    } else {
+        res.status(400).json({ message: 'user does not exist' })
+    }
+    
+})
+
+router.post('/', (req, res) => {
+    const blogPost = req.body;
+
+    if (blogPost.title && blogPost.contents) {
+        DB.insert(blogPost)
+            .then(db => {
+                res.status(200).json(db)
+            })
+            .catch(error => {
+                res.status(500).json({ message: 'error making blogpost' })
+            })
+    } else {
+        res.status(400).json({ message: 'please provide title, and content' })
+    }
+})
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const blogPost = req.body;
+
+    if (blogPost.title && blogPost.contents) {
+        DB.update(id, blogPost)
+            .then(db => {
+                res.status(200).json(db)
+            })
+            .catch(error => {
+                res.status(500).json({ message: 'error updating blogpost' })
+            })
+    } else {
+        res.status(400).json({ message: 'please provide title, and content' })
+    }
+})
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (id) {
+        DB.remove(id)
+            .then(db => {
+                res.status(200).json(db)
+            })
+            .catch(error => {
+                res.status(500).json({ message: 'error deleting user' })
+            })
+    } else {
+        res.status(400).json({ message: 'user does not exist' })
+    }
+})
+
